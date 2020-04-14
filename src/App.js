b60cff1d3bd0cb1fb40eb48a9eb6268468fc8d4d
Shivakumara Bannimatti <shivaslb72@gmail.com>
2020-04-14 21:53:41 +0530

    finished styling a component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
-import HomePage from './home.component';
+
+import HomePage from './pages/homepage/homepage.component';
 
 function App() {
   return (
-    <div >
+    <div>
       <HomePage />
     </div>
   );
---
+++ src/components/pages/homepage/home.component.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import "./home.styles.scss"
+import Directory from "../../directory/directory.component"
+
+const HomePage = () => {
+ return (
+
+  <div className="homepage">
+   <Directory />
+  </div>
+ )
+}
+
+export default HomePage
